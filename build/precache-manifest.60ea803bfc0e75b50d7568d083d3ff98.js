bf60dfc36950fb386e4d4ee502112c8631b66e80
Melody20168066 <Melody20168066@163.com>
2019-05-07 17:53:15 +0800

    更新添加homepage后打包的build文件

---
+++ build/precache-manifest.60ea803bfc0e75b50d7568d083d3ff98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a726f621284fb94a755e69a2cf2f2c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c97c3cffd7ea0b413017",
+    "url": "./static/css/main.68cdeb3a.chunk.css"
+  },
+  {
+    "revision": "a7be114bf65bc2a3d464",
+    "url": "./static/js/2.5bae5401.chunk.js"
+  },
+  {
+    "revision": "c97c3cffd7ea0b413017",
+    "url": "./static/js/main.3e8cb043.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d95d5ab6b4c1b16b815aa5fa4ef6cce9.js"
+  "./precache-manifest.60ea803bfc0e75b50d7568d083d3ff98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
